893d4821278193d6cf90b36e2b08fb477ea547c8
sourdoggy <150556515+sourdoggy@users.noreply.github.com>
2025-08-12 01:53:31 +0300

    Update query_file.mjs

--- api/query_file.mjs
+++ api/query_file.mjs
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   }
 
   const { data, error } = await supabase
-    .from('files')
+    .from('documents')
     .select('*')
     .eq('name', name)
     .eq('password', password)
