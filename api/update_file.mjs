ff71da867bb3cd0c86ab90dd2348d2658c8653c4
sourdoggy <150556515+sourdoggy@users.noreply.github.com>
2025-08-12 22:45:56 +0300

    return unsuccessful for wrong password or filename

--- api/update_file.mjs
+++ api/update_file.mjs
@@ -57,7 +57,11 @@ export default async function handler(req, res) {
   if (error) {
     return res.status(500).json({ error: error.message })
   }
-
+  
+  if (!data || data.length === 0) {
+    return res.status(404).json({ error: 'wrong filename or password' })
+  }
+  
   return res
     .status(200)
     .json({ message: 'file successfully updated', data })
